064271546c16678f2d7dfe617ad276dfa6b62dfc
micko <mickokolius@gmail.com>
2012-10-26 17:30:23 +0300

    add validation

--- public/js/models/company.js
+++ public/js/models/company.js
@@ -1,5 +1,47 @@
 var Company = Backbone.Model.extend({
-    defaults: {
+    validation: {
+        name: {
+            required: true
+            , msg: "Name is required"
+        }
+        , Street: {
+            required: true
+            , msg: "Street is required"
+        }
+        , street_number: [
+            {
+                required: true
+                , msg: "Street number is required"
+            }
+            ,{
+                pattern: /^0-9/
+                , msg: "Only digits is allowed"
+            }
+        ]
+        , city: {
+            required: true
+            , msg: "Phone number is required"
+        }
+        , zip_code: [
+            {
+                required: true
+                , msg: "Phone number is required"
+            }
+            ,{
+                pattern: /^0-9/
+                , msg: "Only digits is allowed"
+            }
+        ]
+    }
+    , labels: {
+        name: "Name"
+        , street: "Street"
+        , street_number: "Street Number"
+        , city: "City"
+        , zip_code: "Zip code"
+        , url : "Url"
+    }
+    , defaults: {
         name: ""
         , street: ""
         , street_number: ""
--- public/js/models/contact.js
+++ public/js/models/contact.js
@@ -1,5 +1,22 @@
 var Contact = Backbone.Model.extend({
-    defaults: {
+    name: {
+        required: true
+        , msg: "Name is required"
+    }
+    , position: {
+        required: true
+        , msg: "Position is required"
+    }
+    , phone_number: {
+        required: true
+        , msg: "Phone number is required"
+    }
+    , labels: {
+        name: "Name"
+        , position: "Position"
+        , phone_number: "Pphone number"
+    }
+    , defaults: {
         name: ""
         , position: ""
         , phone_number: ""
