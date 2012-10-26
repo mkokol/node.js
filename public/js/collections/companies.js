4521de11306ce7ab0e92783125505bf9e8719cce
micko <mickokolius@gmail.com>
2012-10-26 22:14:10 +0300

    fix

--- public/js/collections/companies.js
+++ public/js/collections/companies.js
@@ -1,7 +1,9 @@
 var Companies = Backbone.Collection.extend({
 	model: Company
 	, url: '/company'
+    , total: 0
     , parse: function(data) {
+        this.total = data.total;
         return data.records;
     }
 });
