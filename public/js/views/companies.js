8ec1bc10d9bea50d2fcaba62bd799fa44eb58878
micko <mickokolius@gmail.com>
2012-10-27 10:59:03 +0300

    add

--- public/js/views/companies.js
+++ public/js/views/companies.js
@@ -1,10 +1,12 @@
 var CompaniesView = Backbone.View.extend({
+    pagingTemplate: "../../tmpl/paging.html",
 	initialize: function() {
 		this.collection.on('add', this.addOne, this);
 		this.collection.on('reset', this.render, this);
 	},
 	render: function() {
 		this.collection.forEach(this.addOne, this);
+        this.addPging();
 	},
 	addOne: function(company) {
 		var _this = this;
@@ -15,5 +17,11 @@ var CompaniesView = Backbone.View.extend({
 		companyView.render(function(companyEl) {
 			_this.$el.append(companyEl);
 		});		
-	}
+	},
+    addPging: function(){
+        var total = this.collection.total;
+        fetchTemplate(this.pagingTemplate, function(tmpl){
+//            $("#paging").html(tmpl());
+        });
+    }
 });
